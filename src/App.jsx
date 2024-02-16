7bca46667ae3b18f064392a4c0f54629103c28d7
mintusikder <mintusikder15@gmail.com>
2024-02-16 12:57:08 +0600

    added footer section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 
 import { Outlet } from "react-router-dom";
 import Header from "./component/Header";
+import Footer from "./component/Footer";
 
 const App = () => {
   return (
     <div>
       <Header></Header>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
