ce05dad7dd976abd80e478a82c4d7d34812fa368
mintusikder <mintusikder15@gmail.com>
2024-02-16 14:30:35 +0600

    added book component

--- src/component/Books.jsx
+++ src/component/Books.jsx
@@ -1,6 +1,7 @@
 // import React from 'react';
 
 import { useLoaderData } from "react-router-dom";
+import Book from "./Book";
 
 const Books = () => {
   const { books } = useLoaderData();
@@ -9,21 +10,7 @@ const Books = () => {
     <div className="my-container">
       <div className="grid gap-6 mb-8 lg:grid-cols-4 sm:grid-cols-2">
         {books.map((book) => (
-          <div className="overflow-hidden relative transition duration-200 transform hover:-translate-y-2">
-            {" "}
-            <img
-              src={book.image}
-              alt="book cover"
-              className="object-cover w-full h-56 md:h-64 xl:h-80"
-            />
-            <div className="bg-black px-6 py-4 text-gray-300 bg-opacity-75 absolute inset-0 opacity-0 hover:opacity-100 transition-opacity duration-200 flex flex-col">
-              <p>{book.title}</p>
-              <br />
-              <p>{book.subtitle.substring(0, 45)}</p>
-              <br />
-              <p>Price: {book.price}</p>
-            </div>
-          </div>
+            <Book key={book.id} book={book}></Book>
         ))}
       </div>
     </div>
